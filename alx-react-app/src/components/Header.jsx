f2fd3c1c1fef9c342e9bf22099ecc8190ecf408b
Getasew Abeba <muhamedesmil@gmail.com>
2025-02-15 00:29:53 -0800

    react commit23

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,3 +1,15 @@
-<header>
-  <h1>My Favorite Cities</h1>
-</header>
+
+
+function header() {
+    return (
+        <div>
+        <header>
+    <h1>My Favorite Cities</h1>
+    </header>
+          
+          
+        </div>
+    );
+}
+
+export default header;
