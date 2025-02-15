611fec4974c45a4695d0b85b1d16627d52bdfe97
Getasew Abeba <muhamedesmil@gmail.com>
2025-02-15 00:32:47 -0800

    react commit23

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 
 
-function header() {
+function Header() {
     return (
         <div>
         <header>
@@ -12,4 +12,4 @@ function header() {
     );
 }
 
-export default header;
+export default Header;
