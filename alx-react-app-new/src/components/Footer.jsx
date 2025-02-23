181a5111e9436dd6ad015b12cf4182308235953e
Getasew Abeba <muhamedesmil@gmail.com>
2025-02-23 10:53:16 -0800

    to commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+    return (
+        <div>
+      
+      <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <p>© 2023 City Lovers</p>
+        </footer>
+                
+        </div>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+
+  
+
+
+function MainContent() {
+    return (
+        <div>
+      <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+      
+
+        </div>
+    );
+}
+
+export default MainContent;
