a29673c49dd4558a57961b53ad07c0b2179f29a3
Getasew Abeba <muhamedesmil@gmail.com>
2025-02-23 12:19:18 -0800

    to commit final454

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
+    <nav style={{
+      display: 'flex',
+      justifyContent: 'space-around', // Evenly spaces out the links
+      alignItems: 'center',           // Vertically centers items
+      padding: '10px',
+      backgroundColor: '#333',
+      color: 'white'
+    }}>
       <Link to="/" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Services</Link>
