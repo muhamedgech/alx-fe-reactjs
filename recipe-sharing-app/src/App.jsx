a7a45346624b4bd3db3aee6a16cc2838b458d14a
Getasew Abeba <muhamedesmil@gmail.com>
2025-03-01 23:58:26 -0800

    zustand commit2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import RecipeList from './components/RecipeList' // Capitalize the component names
-import AddRecipeForm from './components/AddRecipeForm' // Capitalize the component names
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
-  
   return (
-    <>
-      <div className='recipe'>
-        
-        <RecipeList />  {/* Capitalized */}
-        <AddRecipeForm />  {/* Capitalized */}
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+        <AddRecipeForm />
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,16 @@
-
+// recipeStore.js
 import { create } from 'zustand';
-
 
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !== id) })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+  setRecipes: (recipes) => set({ recipes })
 }));
 
 export { useRecipeStore };
