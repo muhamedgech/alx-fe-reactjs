935b694c2b5f5f20bbe3fa7fd7bdfcdc68b6644f
Getasew Abeba <muhamedesmil@gmail.com>
2025-03-02 00:13:38 -0800

    zustand commit32

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
@@ -22,6 +23,8 @@ const RecipeList = () => {
             <div key={recipe.id}>
               <h3>{recipe.title}</h3>
               <p>{recipe.description}</p>
+              {/* Link to the Recipe Details page */}
+              <Link to={`/recipe/${recipe.id}`}>View Details</Link>
             </div>
           ))
         ) : (
