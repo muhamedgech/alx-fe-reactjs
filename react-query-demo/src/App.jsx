3ed2cc7d71b2bb791da3a9f34f956cc1f6da16c1
Getasew Abeba <muhamedesmil@gmail.com>
2025-03-08 03:11:15 -0800

    fir commit

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 // Create a QueryClient instance
 const queryClient = new QueryClient();
